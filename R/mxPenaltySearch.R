1508253a44f055607f7142dee40398840ba83f80
tbrick <trb21@HDFS-TRB21-10089.local>
2019-08-06 16:09:29 -0400

    Penalty search now has better options to return reasonable values.

--- R/mxPenaltySearch.R
+++ R/mxPenaltySearch.R
@@ -30,17 +30,22 @@
 #' @param ... the search space for this sequence in format *param=c(...)* where c(...) is the list of possible values for parameter param.
 #' @param approach what fit function to use?  Currently only EBIC is available
 #' @param ebicGamma what Gamma value to use for EBIC?  Must be between 0 and 1
+#' @param returnConstrained fix all regularized-to-zero values to zero, and rerun fit without regularization
+#' 
+#' @details TODO: Figure out how to return all the individual runs.
+#' 
 #' @return A regularized MxModel; at this point an MxRegularizedModel
 #' @import OpenMx
 # @importFrom methods .hasSlot is slotNames
 #' @importFrom stats sd
 #' @export
 #' 
-mxPenaltySearchExternal <- function(model, search_space=NULL, epsilon=1e-6, ..., approach="EBIC", ebicGamma=.5, verbose=TRUE) {
+mxPenaltySearchExternal <- function(model, search_space=NULL, epsilon=1e-6, ..., approach="EBIC", ebicGamma=.5, returnConstrained=FALSE, verbose=TRUE) {
   
   # Check for regularization
-  if(!is(model, "MxRegularizedModel")) {
-    stop("Not a regularized model.")
+  if(!is(model, "MxRegularizedModel") || length(model@regularizations) < 1) {
+    warning("Not a regularized model.")
+    return(mxRun(model))
   }
   
   # Find all the hyperparameters in each penalty object
@@ -50,20 +55,24 @@ mxPenaltySearchExternal <- function(model, search_space=NULL, epsilon=1e-6, ...,
     thisName <- penalty@name
     penMat <- paste0(thisName, "_Penalty")
     for(aVal in names(penalty@hyperparameters)) {
-      if(aVal %in% names(search_space)) {
-        thisParam <- data.frame(search_space[[aVal]])
+      if(is.null(search_space) || aVal %in% names(search_space)) {
+        if(is.null(search_space)) {
+          thisParam <- data.frame(penalty@hpranges[[aVal]])
+        } else {
+          thisParam <- data.frame(search_space[[aVal]])
+        }
         names(thisParam) <- c(paste0(thisName, "_", aVal))
         if(is.null(penaltySearch)) {
           penaltySearch <- thisParam
         } else {
-          penaltySearch <- cbind(penaltySearch[rep(1:nrow(penaltySearch), length(thisParam)),], thisParam[rep(1:nrow(thisParam), each=nrow(penaltySearch)), ])
+          penaltySearch <- suppressWarnings(cbind(penaltySearch[rep(1:nrow(penaltySearch), length(thisParam)), 1:ncol(penaltySearch), drop=FALSE], thisParam[rep(1:nrow(thisParam), each=nrow(penaltySearch)), 1:ncol(thisParam),drop=FALSE])) # Suppress to avoid row-name-dropping warnings
         }
       }
     }
   }
 
   getEBIC <- function(idx, model, search_space, ..., gamma=ebicGamma, pb=NULL) {
-    amodel <- omxSetParameters(model, labels=names(search_space), values=search_space[idx,])
+    amodel <- omxSetParameters(model, labels=names(search_space), values=as.numeric(search_space[idx,]))
     amodel <- mxOption(amodel, "Calculate Hessian", "No")
     amodel <- mxOption(amodel, "Standard Errors"  , "No")
     tFit <- mxRun(amodel, silent=TRUE, unsafe=TRUE) #, beginMessage=FALSE, suppressWarnings=TRUE)
@@ -102,9 +111,44 @@ mxPenaltySearchExternal <- function(model, search_space=NULL, epsilon=1e-6, ...,
     pb <- txtProgressBar(min = 0, max = nrow(penaltySearch), style = 3)
   }
   
-  outList <- sapply(1:nrow(penaltySearch), getEBIC, search_space=penaltySearch, model=model, gamma=ebicGamma, pb=pb)
+  outList <- sapply(1:nrow(penaltySearch), getEBIC, search_space=penaltySearch, epsilon=epsilon, model=model, gamma=ebicGamma, pb=pb)
   
   outList <- data.frame(t(outList))
+  
+  if(returnConstrained) {
+    minmod <- unlist(outList[which.min(outList$EBIC),])
+    fixed <- abs(minmod) < epsilon
+    minmod[fixed] <- 0
+    names(fixed) <- names(minmod)
+    tries <- outList
+    tParms <- omxGetParameters(model, free=TRUE)
+    combinedNames <- intersect(names(minmod), names(tParms))
+    tmodel <- omxSetParameters(model, labels=combinedNames, 
+                               values=minmod[combinedNames], 
+                               free=fixed[combinedNames])
+    tmodel <- OpenMx::mxModel(tmodel, mxComputeDefault())
+    outList <- mxTryHard(tmodel)
+    otherNames <- setdiff(names(minmod), names(tParms))
+    message("Final solution has params ", paste(names(omxGetParameters(outList)), "=", omxGetParameters(outList), collapse=", "), " and a likelihood of ", paste0(summary(outList)$Minus2LogLikelihood), " at reg levels ", paste0(otherNames, ":", minmod[otherNames], collapse=", "), "and an N of ", outList$data$numObs)
+    outList@output <-c(outList@output, list("regularizationSearchOutcomes"=tries))
+  } else {
+    minmod <- unlist(outList[which.min(outList$EBIC),])
+    tries <- outList
+    tRegs <- names(penaltySearch)
+    tParms <- omxGetParameters(model, free=TRUE)
+    combinedNames <- intersect(names(minmod), names(tParms))
+    combinedNames <- setdiff(combinedNames, tRegs)
+    tmodel <- omxSetParameters(model, labels=combinedNames, 
+                               values=minmod[combinedNames], 
+                               free=TRUE)
+    tmodel <- omxSetParameters(tmodel, labels=tRegs, 
+                               values=minmod[tRegs], 
+                               free=FALSE)
+    outList <- mxTryHard(tmodel)
+    otherNames <- setdiff(names(minmod), names(tParms))
+    message("Final solution has params ", paste(names(omxGetParameters(outList)), "=", omxGetParameters(outList), collapse=", "), " and a likelihood of ", paste0(summary(outList)$Minus2LogLikelihood), " at reg levels ", paste0(otherNames, ":", minmod[otherNames], collapse=", "), "and an N of ", outList$data$numObs)
+    outList@output <-c(outList@output, list("regularizationSearchOutcomes"=tries))
+  }
   
   # Trying to reintegrate into an openMx model output.
   # minmod <- unlist(outList[which.min(outList$EBIC),])
@@ -116,7 +160,12 @@ mxPenaltySearchExternal <- function(model, search_space=NULL, epsilon=1e-6, ...,
   #                                               c('expectation',
   #                                                 'fitfunction')
   #                           mxComputeStandardError(),)))
+
+  return(outList)
 } 
+
+
+
 
 #' Search for an optimal penalty value  **Experimental**
 #'
@@ -164,5 +213,14 @@ mxPenaltySearch <- function(model, search_space=NULL, epsilon=1e-6, ...) {
   }
   
   # Complete compute list
+  model <- mxModel(model, 
+   mxComputeSequence(steps=list(
+    mxComputeOnce('fitfunction', 'fit'),
+    mxComputeNumericDeriv(parallel=FALSE, iterations=2L),
+    mxComputeStandardError(),
+    mxComputeHessianQuality(),
+    mxComputeReportDeriv())
+   )
+  )
   
 }
